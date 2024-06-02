309a692a3a26ec56ac3fac1d7fdf1a6d58095f7f
Hugo Barbosa <hugoribarbosa@gmail.com>
2024-06-02 17:14:45 +0100

    Add Flyweight design pattern
    - Add Flyweight design pattern documentation

---
+++ designPatterns/Flyweight/src/main.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * @copyright Copyright (c) 2024.
+ */
+
+#include <iostream>
+
+/**
+ * @brief Main function.
+ *
+ * @return Program exit code.
+ */
+int main()
+{
+    std::cout << "== Flyweight ====" << std::endl;
+
+    // TODO: Complete the code.
+
+    return EXIT_SUCCESS;
+}
